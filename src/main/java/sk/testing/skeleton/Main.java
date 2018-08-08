8e0f55708c68d9b376cbbf2ddbea5858d1a89e92
robin1205 <42021867+robin1205@users.noreply.github.com>
2018-08-08 16:47:24 +0900

    fb3-3

--- src/main/java/sk/testing/skeleton/Main.java
+++ src/main/java/sk/testing/skeleton/Main.java
@@ -2,15 +2,4 @@ package sk.testing.skeleton;
 
 public class Main {
 //	blah blah blah
-//	add second line
-//	fix #2
-//	check for #2
-//	테스트
-//	Additional Raw
-
-// hotfix #1
-//    from server2
 }
-123
-fb4-2
-fb2-2
